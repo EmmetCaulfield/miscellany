7ff050a4fd92b7d626040cefe31fdab12c77ad31
Emmet Caulfield <emmet.caulfield@stanford.edu>
2015-05-27 13:49:15 -0700

    Added "debugg" target to build a debug binary with -Og optimization per gcc manual recommendation.

--- make/bits/debug.mk
+++ make/bits/debug.mk
@@ -1,9 +1,12 @@
 #===[ bits/debug.mk ]==================================================
-# debug, debug0: make debug binaries.
+# debug, debug0. debugg: make debug binaries.
 #
 # The 'debug' target leaves optimization alone, while the 'debug0'
-# target disables optimization. Both targets remove -DNDEBUG, if
-# present, which disables assert() macros.
+# target disables optimization, and the 'debugg' target enables -Og,
+# the recommended optimization level for development. All three
+# targets remove -DNDEBUG, if present, which disables assert() macros,
+# so that assert macros() are enabled.
+# ----------------------------------------------------------------------
 ifneq (,$(filter debug,$(MAKECMDGOALS)))
 CPPFLAGS:=$(subst -DNDEBUG,,$(CPPFLAGS))
 DBUG_FLAGS:=$(DBUG_OPTS)
@@ -18,4 +21,13 @@ DBUG_FLAGS:=$(DBUG_OPTS)
 LDFLAGS+=$(DBUG_OPTS)
 endif
 debug0: build
+
+ifneq (,$(filter debugg,$(MAKECMDGOALS)))
+CPPFLAGS:=$(subst -DNDEBUG,,$(CPPFLAGS))
+OPTM_FLAGS:=$(filter-out -O%,$(OPTM_FLAGS))
+OPTM_FLAGS+=-Og
+DBUG_FLAGS:=$(DBUG_OPTS)
+LDFLAGS+=$(DBUG_OPTS)
+endif
+debugg: build
 #======================================================================
