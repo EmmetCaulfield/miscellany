01623d51ba43a00713fcc42f9c4f90c3a0521f57
Emmet Caulfield <emmet.caulfield@stanford.edu>
2015-05-28 14:11:30 -0700

    New 'verbose' utility target was unconditional!

--- make/bits/verbose.mk
+++ make/bits/verbose.mk
@@ -1,6 +1,8 @@
 #===[ bits/verbose.mk ]==================================================
 # verbose: produce verbose compiler output.
 # ----------------------------------------------------------------------
+ifneq (,$(filter verbose,$(MAKECMDGOALS)))
 BHVR_FLAGS+=-v
+endif
 verbose: build
 #======================================================================
