284defd474c2c67d291a668ec5c7080e005eff5c
Emmet Caulfield <emmet.caulfield@stanford.edu>
2015-06-11 16:50:35 -0700

    Renamed the sample input/output files.

--- awk/mopal/sample.m
+++ awk/mopal/sample.m
@@ -15,20 +15,16 @@ b1=ones(1, numClasses);
 
 % We're only interested in the matrix dimensions, so we don't bother
 % with realistic functions, only ones that are dimensionally correct:
-colsum  = @(x) sum(x,1);
-rowsum  = @(x) sum(x,2);
 sigmoid = @(x) x;
 softmax = @(x) x;
-tr      = @(x) x';
 lambda  = 1;
 rho     = 1;
 
 
 % -mopal-start-ANN
 % -mopal-group-feedForward
-
 T    = X                      ;
-R    = tr(W0)                 ;
+R    = transpose(W0)          ;
 T   *= R                      ;
 R    = repmat(b0,batchSize,1) ;
 T   += R                      ;
@@ -37,7 +33,7 @@ R    = sigmoid(z0)            ;
 a0   = R                      ;
 
 T    = a0                     ;
-R    = tr(W1)                 ;
+R    = transpose(W1)          ;
 T   *= R                      ;
 R    = repmat(b1,batchSize,1) ;
 T   += R                      ;
@@ -49,68 +45,71 @@ yc   = T                      ;
 % -mopal-end-feedForward
 
 % -mopal-group-backPropagate
-T      = yc           ;
-T     -= y            ;
-T     /= batchSize    ;
+T      = yc              ;
+T     -= y               ;
+T     /= batchSize       ;
 
-diff   = T            ;
-T      = tr(diff)     ;
-T     *= a0           ;
-R      = W1           ;
-R     *= lambda       ;
-T     += R            ;
-dW1    = T            ;
+diff   = T               ;
+T      = transpose(diff) ;
+T     *= a0              ;
+R      = W1              ;
+R     *= lambda          ;
+T     += R               ;
+dW1    = T               ;
 
-T      = colsum(diff) ;
-db1    = T            ;
+T      = sum(diff,1)     ;
+db1    = T               ;
 
-T      = diff         ;
-T     *= W1           ;
-da1    = T            ;
+T      = diff            ;
+T     *= W1              ;
+da1    = T               ;
 
-T      = da1          ;
-T    .*= a0           ;
-R      = 1            ;
-R     -= a0           ;
-T    .*= R            ;
-dz1    = T            ;
+T      = da1             ;
+T    .*= a0              ;
+R      = 1               ;
+R     -= a0              ;
+T    .*= R               ;
+dz1    = T               ;
 
-T      = tr(dz1)      ;
-T     *= X            ;
-R      = W0           ;
-R     *= lambda       ;
-T     += R            ;
-dW0    = T            ;
+T      = transpose(dz1)  ;
+T     *= X               ;
+R      = W0              ;
+R     *= lambda          ;
+T     += R               ;
+dW0    = T               ;
 
-T      = colsum(dz1)  ;
-db0    = T            ;
+T      = sum(dz1,1)      ;
+db0    = T               ;
 
 % -mopal-end-backPropagate
 % -mopal-group-descend
-
+% -mopal-group-W0
 T    = W0  ;
 R    = dW0 ;
 R   *= rho ;
 T   -= R   ;
 W0   = T   ;
-
+% -mopal-end-
+% -mopal-group-b0
 T    = b0  ;
 R    = db0 ;
 R   *= rho ;
 T   -= R   ;
 b0   = T   ;
-
+% -mopal-end-
+% -mopal-group-W1
 T    = W1  ;
 R    = dW1 ;
 R   *= rho ;
 T   -= R   ;
 W1   = T   ;
-
+% -mopal-end-
+% -mopal-group-b1
 T    = b1  ;
 R    = db1 ;
 R   *= rho ;
 T   -= R   ;
 b1   = T   ;
-
+% -mopal-end-
 % -mopal-end-descend
 % -mopal-end-ANN
