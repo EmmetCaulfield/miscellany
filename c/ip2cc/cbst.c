0c562c959d4c2a3cda90c1971492df4436b8ecaf
Emmet Caulfield <emmet.caulfield@stanford.edu>
2018-09-29 12:49:30 -0700

    Added experimental alternative cbst_index() function.

--- c/ip2cc/cbst.c
+++ c/ip2cc/cbst.c
@@ -2,57 +2,85 @@
 
 #include <stdlib.h>     // for malloc()
 #include <string.h>     // for memcpy()
+#include <sys/param.h>  // for correct MIN()/MAX() macros
+
+#include <stdio.h>      // for printf()
  
 // Heap/tree movement:
 static inline int left(int i)   { return (i<<1)+1; }
 static inline int right(int i)  { return (i<<1)+2; }
 static inline int parent(int i) { return (i-1)>>1; }
 
-// Bit position (0-31) of highest set bit:
-static inline unsigned hsb(unsigned i) {
-    return 8*sizeof(int)-1-__builtin_clz(i);
+// Zoom left n steps:
+static inline int weft(int i, int n) { return (i<<n)+(1<<n)-1; }
+// Zoom right n steps:
+static inline int warp(int i, int n) { return (i<<n)+(2<<n)-2; }
+
+static inline int is_p2(int i) {
+    return __builtin_popcount(i)==1;
 }
 
-// Raise to the power of 2:
-static inline unsigned p2(unsigned i) {
-    return 1<<i;
+// Height of binary tree given size n
+static inline unsigned height(unsigned n) {
+    return 8*sizeof(unsigned)-__builtin_clz(n);
 }
 
-// Greatest power-of-two less than or equal to
-static inline unsigned gp2le(unsigned i) {
-    return i==0 ? 0 : 1 << hsb(i);
-}
-
-// Level in CBST of index, counting from zero at the top:
-static inline unsigned level(unsigned i) {
-    return hsb( gp2le(i) );
-}
+// Conditional printing:
+static int cprint_on=0;
+#define cprintf(...) if(cprint_on) { printf(__VA_ARGS__); }
 
 
-#define MAX(x,y) (x>y ? x : y)
-#define MIN(x,y) (x<y ? x : y)
-
-// Compute the value at the root (index=0) of a CBST, populated with
-// the integers from 0 to (n-1), given the size, n:
-size_t cbst_root(size_t nmemb) 
+size_t cbst_root(size_t n) 
 {
-    int lT=level(nmemb); // Top level, where the root is, based on
-                         // zero on the bottom; also one less than the
-                         // total number of levels (height)
+    if( n==1 ) {
+        return 0;
+    }
+    size_t h = height(n);    // Height of BST
+    size_t i = (1<<(h-1))-1; // Index of zero element
+    size_t w = n-i;          // Occupied width of the bottom level
+    size_t k = 1<<(h-2);     // Half-capacity of bottom level
+    size_t r = i - MAX((ssize_t)(k-w),0);
 
-    int i0=p2(lT)-1;     // Index of zero element
+    cprintf("[%2zu %6zu %6zu %6zu %6zd %6zu]", h, i, k, w, k-w, r);
 
-    int wB=nmemb-i0;     // Occupied width of the bottom level
-
-    // k here is half the capacity of the bottom level (l0), the
-    // capacity of the 2nd last level (l1), and one more than the
-    // capacity of the 2nd top through 2nd last levels ("left
-    // triangle").
-    int k  = 1<<(lT-1);
-    int vR = (k-1) + MIN(wB,k);    // Value at the root
-
-    return MAX(vR,0);
+    return r;
 }
+
+
+size_t cbst_index2(size_t nmemb, size_t value)
+{
+    size_t saved = value;
+    size_t index = 0;
+    size_t mask  = 1;
+    size_t contra = 1;
+
+    // Commute trailing ones to zeros:
+//    while( mask & value ) {
+//        value ^=mask;
+//        mask <<= 1;
+//    }
+
+    mask = 1 << (height(nmemb)-1);
+    cprintf("{%6zu (%6zu %5zx) (%6zu %5zx) (%6zu %6zu) %d}\n", saved, value, value, index, index, mask, contra, !!(mask&value));
+    while( mask ) {
+        if( mask & value ) {
+            index  = right(index);
+            value ^= mask; // Clear the bit we just tested
+        } else {
+            index = left(index);
+        }
+        mask >>= 1;
+        contra <<= 1;
+        cprintf("{%6zu (%6zu %5zx) (%6zu %5zx) (%6zu %6zu) %d}\n", saved, value, value, index, index, mask, contra, !!(mask&value));
+        if( value == mask-1 ) {
+            break;
+        }
+    }
+
+    cprintf("Final: {%6zu (%6zu %5zx) (%6zu %5zx) %5zx %zx}\n", nmemb, value, value, index, index, mask, mask&value);
+    return index;
+}
+
 
 // Compute the index in the CBST of size 'size' of a given 'value':
 size_t cbst_index(size_t nmemb, size_t value)
@@ -60,25 +88,47 @@ size_t cbst_index(size_t nmemb, size_t value)
     size_t index=0;
     size_t root=1;
 
+    size_t test=cbst_index2(nmemb, value);
+
+    if(cprint_on) {
+        cprint_on=1;
+    }
+
+    printf("\ncbst_index(%zu, %zu) %zx\n", nmemb, value, nmemb^value);
     while( root ) {
         root = cbst_root(nmemb);
-//        printf("\t%zu ?= %zu ", value, root);
+/*
+        if( is_p2(root+1) ) {
+            cprintf("  ");
+        } else {
+            cprint_on++;
+            cprintf(" @%d", cprint_on);
+        }
+*/
+        cprintf("\t%zu ?= %zu (%zu, %zu)", value, root, nmemb, index);
         if( value > root ) {
             // Go right
-//            printf(" <- \n");
+            cprintf(" -> \n");
             index  = right(index);
             nmemb -= root + 1;
             value -= root + 1;
         } else if( value < root ) {
             // Go left
-//            printf(" -> \n");
+            cprintf(" <- \n");
             index = left(index);
             nmemb = root;
         } else {
+            cprintf(" == \n");
             // value == root
             break;
         }
     }
+
+    if( index!=test ) {
+        cprint_on = 1;
+    }
+
+    cprintf( "%s %zu (%zu)\n\n", (index==test ? "BOOM!" : "Wah!!"), index, test);
     return index;
 }
 
